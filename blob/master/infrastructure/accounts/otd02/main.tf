975181572a0c0ba9fc1783bbe3fdd825e2953761
Miu Daria <93943018+dariamiu@users.noreply.github.com>
2025-08-14 12:43:23 +0300

    Create main test

---
+++ blob/master/infrastructure/accounts/otd02/main.tf
@@ -0,0 +1,10 @@
+module one {
+source = "git::ssh://git@aa/bb/repoccc.git//somelib?ref=1.5.3"
+}
+
+something
+something
+
+module two {
+source = "git::ssh://git@dd/ee/repoff.git//somelib?ref=1.5.3"
+}
