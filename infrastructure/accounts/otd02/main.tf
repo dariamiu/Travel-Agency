816a46ac04d22e7ef5b54b1dc724884b06ecacf7
dariamiupartner <daria.miu@partner.bmw.de>
2025-08-19 10:26:13 +0300

    Update versions

--- infrastructure/accounts/otd02/main.tf
+++ infrastructure/accounts/otd02/main.tf
@@ -1,10 +1,10 @@
 module one {
-source = "git::ssh://git@aa/bb/repoccc.git//somelib?ref=1.5.3"
+source = "git::ssh://git@aa/bb/repoccc.git//somelib?ref=1.7.0"
 }
 
 something
 something
 
 module two {
-source = "git::ssh://git@dd/ee/repoff.git//somelib?ref=1.5.3"
+source = "git::ssh://git@dd/ee/repoff.git//somelib?ref=1.7.0"
 }
