8623dead06d654118d000a584c7e6eb273eb0715
dariamiupartner <daria.miu@partner.bmw.de>
2025-08-20 17:37:22 +0300

    Update versions

--- infrastructure/accounts/otd02/main.tf
+++ infrastructure/accounts/otd02/main.tf
@@ -13,6 +13,7 @@ module "three" {
     source = "git::ssh://git@dd/ee/repoff.git//semantic-emr?ref=1.7.0"
 }
 
+
 module "itsm_alert_config" {
   count  = var.stage == "prod" ? 1 : 0
   source = "git::ssh://git@atc-github.azure.cloud.bmw/DAOTD/itsm-mapping-generator.git//infrastructure?ref=v1.0.0"
@@ -22,6 +23,18 @@ module "itsm_alert_config" {
     {
       module_name         = "one"
       job_names           = ["one"]
+      implementation_type = module.itsm_alert_config[0].allowed_implementation_types.sem_emr
+    },
+
+    {
+      module_name         = "two"
+      job_names           = ["two"]
+      implementation_type = module.itsm_alert_config[0].allowed_implementation_types.pre_glue
+    },
+
+    {
+      module_name         = "three"
+      job_names           = ["three"]
       implementation_type = module.itsm_alert_config[0].allowed_implementation_types.sem_emr
     }
 
